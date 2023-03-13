7049e4fc381786ea9a41c5de7ae4c875b07694b5
Andrii Domchuk <andriy28domchuk@gmail.com>
2023-03-13 12:51:24 +0200

    Update
    add for and multiplyPrices method

--- java/UberShop.java
+++ java/UberShop.java
@@ -1,18 +1,32 @@
 import java.util.Arrays;
 
 public class UberShop {
-    public void printPrices(float[] prices) {
-        for (float i = 0; i < prices.length; i++) {
-            System.out.println(prices[(int) i] + " jup.");
+    public void printPrices(float[] pricesPrint) {
+        for (float i = 0; i < pricesPrint.length; i++) {
+            System.out.println(pricesPrint[(int) i] + " jup.");
         }
+//        for (float price: prices) {
+//            System.out.println(price + " jup.");
+//        }
     }
 
-    //Test output
+    public void  multiplyPrices(float[] prices) {
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < 1000) {
+                prices[i] *= 2;
+            } else {
+                prices[i] *= 1.5;
+            }
+        }
+    }
     public static void main(String[] args) {
         UberShop shop = new UberShop();
 
-        //Should be 100 jup. 23.5 jup. 400 jup. - one price per line
-        float[] prices = new float[] {100f, 23.5f, 400f};
-        shop.printPrices(prices);
+        float[] pricesPrint = new float[] {100f, 23.5f, 400f};
+        shop.printPrices(pricesPrint);
+
+        float[] prices = new float[] {100f, 1500f};
+        shop.multiplyPrices(prices);
+        System.out.println(Arrays.toString(prices));
     }
 }
