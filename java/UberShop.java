a5a68a4f8a5aa76d4c10cb16f9f5c0504a4b2a09
Andrii Domchuk <andriy28domchuk@gmail.com>
2023-03-14 08:58:20 +0200

    update
    Add method findMinMaxPrices

--- java/UberShop.java
+++ java/UberShop.java
@@ -19,14 +19,50 @@ public class UberShop {
             }
         }
     }
+
+    public int[] findMinMaxPrices(int[] prices) {
+        Arrays.sort(prices);
+        int[] resultSort = new int[2];
+
+            resultSort[0] = prices[0];
+            resultSort[1] = prices[prices.length - 1];
+
+            if (resultSort[0] == resultSort[1]) {
+                return new int[]{resultSort[0]};
+            }
+
+        return resultSort;
+//-------------------------------------------------------------
+//        int[] result = new int[2];
+//
+//        if (prices.length == 0) {
+//            return new int[] {};
+//        }
+//
+//        int minPrice = prices[0];
+//        int maxPrice = prices[0];
+//
+//        for (int i = 1; i < prices.length; i++) {
+//            if (prices[i] < minPrice) {
+//                minPrice = prices[i];
+//            } else if (prices[i] > maxPrice) {
+//                maxPrice = prices[i];
+//            }
+//        }
+//
+//        if (minPrice == maxPrice) {
+//            result = new int[] {minPrice};
+//        } else {
+//            result[0] = minPrice;
+//            result[1] = maxPrice;
+//        }
+//        return result;
+    }
     public static void main(String[] args) {
         UberShop shop = new UberShop();
 
-        float[] pricesPrint = new float[] {100f, 23.5f, 400f};
-        shop.printPrices(pricesPrint);
-
-        float[] prices = new float[] {100f, 1500f};
-        shop.multiplyPrices(prices);
-        System.out.println(Arrays.toString(prices));
+        int[] prices = new int[] {};
+        int[] minMax = shop.findMinMaxPrices(prices);
+        System.out.println(Arrays.toString(minMax));
     }
 }
