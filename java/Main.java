f9c0caef1f1843b29fa82c7674bb073ac5bbef5e
Andrii Domchuk <andriy28domchuk@gmail.com>
2023-03-17 19:26:38 +0200

    test

--- java/Main.java
+++ java/Main.java
@@ -1,15 +1,28 @@
 import classes.ATM;
+import classes.FinalTestClass;
 import classes.TrurlBank;
+
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        TrurlBank bank = new TrurlBank();
-        ATM atm = new ATM();
+//        TrurlBank bank = new TrurlBank();
+//        ATM atm = new ATM();
+//
+//        System.out.println(bank.sumQuads(3));
+//        System.out.println(bank.countSumOfDigits(547));
+//
+//        System.out.println(atm.countBanknotes(500));
 
-        //Should be 14 - 1 + 4 + 9
-        System.out.println(bank.sumQuads(3));
-        System.out.println(bank.countSumOfDigits(547));
+        FinalTestClass shop = new FinalTestClass();
 
-        System.out.println(atm.countBanknotes(500));
+//        //Should be 100 jup. 23.5 jup. 400 jup. - one price per line
+//        float[] prices = new float[] {100f, 23.5f, 400f};
+//        shop.printPrices(prices);
+        float[] prices = new float[] {100f, 1500f};
+        shop.multiplyPrices(prices);
+        System.out.println(Arrays.toString(prices));
+
+
     }
 }
--- java/classes/FinalTestClass.java
+++ java/classes/FinalTestClass.java
@@ -1,4 +1,21 @@
 package classes;
 
+import java.util.Arrays;
+
 public class FinalTestClass {
+    public void printPrices(float[] prices) {
+        for (int i = 0; i < prices.length; i++) {
+            System.out.println(prices[i] + " jup.");
+        }
+    }
+
+    public void multiplyPrices(float[] prices) {
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < 1000) {
+                prices[i] *= 2;
+            } else {
+                prices[i] *= 1.5;
+            }
+        }
+    }
 }
