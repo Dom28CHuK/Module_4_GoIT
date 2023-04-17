05fa7078709cd93c7fea0e587ea6c720f21cca3a
Andrii Domchuk <andriy28domchuk@gmail.com>
2023-04-17 12:26:33 +0300

    update

--- java/Main.java
+++ java/Main.java
@@ -7,7 +7,14 @@ public class Main {
         ForPractice forPractice = new ForPractice();
 
 //        System.out.println(forPractice.evenNumbers(100));
-        forPractice.multiplicationTable();
+//        forPractice.multiplicationTable();
+
+//        float[] prices = new float[] {100f, 23.5f, 400f};
+//        forPractice.printPrices(prices);
+
+          float[] price = new float[] {100f, 1500f};
+          forPractice.multiplyPrices(price);
+        System.out.println(Arrays.toString(price));
 
 
 
